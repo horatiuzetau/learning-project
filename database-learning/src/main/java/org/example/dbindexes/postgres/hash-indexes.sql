309abb3773e19703aaef81d4f61de66170cc5569
Atanasoaei Horatiu <horatiu@luxon-tech.com>
2023-09-03 18:00:51 +0300

    rename table from hash-indexes.sql; create multicolumn-indexes.sql file

--- database-learning/src/main/java/org/example/dbindexes/postgres/hash-indexes.sql
+++ database-learning/src/main/java/org/example/dbindexes/postgres/hash-indexes.sql
@@ -1,5 +1,7 @@
+SET search_path TO learning;
+
 -- Testing how hash indexes work
-create table test_index_table
+create table test_hash_index_table
 (
     id             bigserial PRIMARY KEY,
     uq             bigint UNIQUE,
@@ -13,83 +15,83 @@ create table test_index_table
 -- The documentation states that unique indexes can only be created as B-TREES
 
 -- I thought it's a good idea to have it on the ID and I tried doing it:
-create index idx_hash_test_index_table_id
-    on test_index_table using hash (id);
+create index idx_hash_test_hash_index_table_id
+    on test_hash_index_table using hash (id);
 
--- output: "Index Scan using idx_hash_test_index_table_id on test_index_table"
--- This means that the idx_hash_test_index_table_id index is indeed used and that findById will run in O(1)
+-- output: "Index Scan using idx_hash_test_hash_index_table_id on test_hash_index_table"
+-- This means that the idx_hash_test_hash_index_table_id index is indeed used and that findById will run in O(1)
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where id = 5;
 
 
 -- If using the id with operators like > or <, then the PK index is used
--- output: "Bitmap Index Scan on test_index_table_pkey"
+-- output: "Bitmap Index Scan on test_hash_index_table_pkey"
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where id >= 5;
 
 
 -- Example 2 - using hash index on a unique column
 ------------------------------------------------------------
-create index idx_hash_test_index_table_uq_indexed
-    on test_index_table using hash (uq_indexed);
+create index idx_hash_test_hash_index_table_uq_indexed
+    on test_hash_index_table using hash (uq_indexed);
 
--- output: "Index Scan using idx_hash_test_index_table_uq on test_index_table"
+-- output: "Index Scan using idx_hash_test_hash_index_table_uq on test_hash_index_table"
 -- This means that also in this case, the hash index we created is getting used
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where uq_indexed = 1;
 
 
--- output: "Bitmap Index Scan on test_index_table_uq_indexed_key"
+-- output: "Bitmap Index Scan on test_hash_index_table_uq_indexed_key"
 -- This means that the unique index is used here, but if we're using equality, the hash index is used.
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where uq_indexed > 1;
 
 
 -- Example 3 - using hash index on a non-unique column
 ------------------------------------------------------------
-create index idx_hash_test_index_table_non_uq_indexed
-    on test_index_table using hash (non_uq_indexed);
+create index idx_hash_test_hash_index_table_non_uq_indexed
+    on test_hash_index_table using hash (non_uq_indexed);
 
--- output: "Bitmap Index Scan on idx_hash_test_index_table_non_uq_indexed"
+-- output: "Bitmap Index Scan on idx_hash_test_hash_index_table_non_uq_indexed"
 -- This means that for this NON UNIQUE column, the hash index was used successfully.
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where non_uq_indexed = 5;
 
--- output: "Seq Scan on test_index_table"
+-- output: "Seq Scan on test_hash_index_table"
 -- No index is used, because we don't have any defined.
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where non_uq_indexed > 1;
 
 
 -- Example 4: Using uq not indexed
 ------------------------------------------------------------
--- output: Index Scan using test_index_table_uq_key on test_index_table
+-- output: Index Scan using test_hash_index_table_uq_key on test_hash_index_table
 -- NOTE! Also here we can't see Bitmap keyword, but we can be pretty sure that the uq index is used.
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where uq = 5;
 
 
 -- Example 5: Using non_uq not indexed
 ------------------------------------------------------------
--- output: Seq Scan on test_index_table
+-- output: Seq Scan on test_hash_index_table
 -- Meaning it's not using any index
 explain
 select *
-from test_index_table
+from test_hash_index_table
 where non_uq = 5;
 
 
